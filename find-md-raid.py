d48c7069cfdd41ebeb49c3dd2e64df6b00ffb549
Peter Gerber <peter@arbitrary.ch>
2017-08-07 14:13:28 +0200

    python: honor 512-bit alignment
    This increases the speed quite a bit:
    • Python before: ~3.6 MiB/s
    • Python now: ~472 MiB/s
    • Rust: ~3010 MiB/s

--- find-md-raid.py
+++ find-md-raid.py
@@ -3,13 +3,9 @@ import itertools
 import io
 import sys
 
-# 'a92b4efc'
-with open(sys.argv[1], 'rb') as f:
+with open(sys.argv[1], 'rb') as f, io.BufferedReader(f) as f:
     offset = 0
-    last_four_bytes = 0
-    for buffer in iter(lambda: f.read(1048576), b''):
-        for byte in buffer:
-            last_four_bytes = last_four_bytes >> 8 | (byte << 24)
-            if last_four_bytes == 0xa92b4efc:
-                print("hit at byte {}".format(offset - 3))
-            offset += 1
+    for buffer in iter(lambda: f.read(512), b''):
+        if buffer.startswith(b'\xfc\x4e\x2b\xa9'):
+                print("hit at byte {}".format(offset))
+        offset += 512
